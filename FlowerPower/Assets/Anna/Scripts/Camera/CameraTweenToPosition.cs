ec67b46848f5d75caddeed94353f1d995b9c4341
ANNATOOKEY\annat <anna.tookey@hotmail.com>
2019-02-20 20:19:02 +0400

    Added my own scenes/folder structure. Created a camera movement script which slowly follows the player.

---
+++ FlowerPower/Assets/Anna/Scripts/Camera/CameraTweenToPosition.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTweenToPosition : MonoBehaviour
+{
+    public Rigidbody playerReference;
+
+    [SerializeField]
+    Vector3 startPositionCamera;
+    Vector3 cameraPosition;
+    Vector3 cameraOffset;
+    
+    void Start()
+    {
+        playerReference = FindObjectOfType<Rigidbody>();
+        startPositionCamera = transform.position - playerReference.transform.position;
+    }
+
+    void FixedUpdate()
+    {
+        cameraOffset = playerReference.position + startPositionCamera;
+        transform.position = Vector3.Lerp(transform.position, cameraOffset, 0.1f);
+       Debug.DrawLine(playerReference.position, cameraOffset);
+    }
+}
