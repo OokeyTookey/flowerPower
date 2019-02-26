cf94275a5f42757c2cc73480b78053c3f454c650
ANNATOOKEY\annat <anna.tookey@hotmail.com>
2019-02-26 20:55:08 +0400

    Working on making player movement perfect

--- FlowerPower/Assets/Anna/Scripts/AnnaPlayerMovement.cs
+++ FlowerPower/Assets/Anna/Scripts/AnnaPlayerMovement.cs
@@ -5,34 +5,79 @@ using UnityEngine;
 public class AnnaPlayerMovement : MonoBehaviour
 {
     Rigidbody RB;
+    Vector3 movement;
+
     public float speed;
+    const float GRAVITY = 9.8f;
+
+    float moveXAxis;
+    float moveYAxis;
+    public float projectileForce;
+    public float maxJumpForce;
+
+    [SerializeField]
+    float angle;
+    float initalVelocity;
+    float initalVelocityY;
+    float initalVelocityZ;
+    Vector3 comboVelo;
 
     void Start()
     {
         RB = this.GetComponent<Rigidbody>();
+
+        if (RB == null)
+        {
+            Debug.LogError("Rigidbody not found!! -A");
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (RB != null)
+        {
+            movement = new Vector3(moveXAxis, 0, moveYAxis) * speed;
+            RB.AddForce(movement, ForceMode.Acceleration); //Adds a continuous force, utilizing the mass of the object
+            //Add Force parameter; Acceleration, Force, Impulse, and VelocityChange.
+        }
     }
 
     void Update()
     {
-        if (Input.GetKey(KeyCode.W))
-        {
-            print("fikjc");
-            RB.AddForce(transform.forward * speed);
-        }
+        moveXAxis = Input.GetAxis("Horizontal");
+        moveYAxis = Input.GetAxis("Vertical");
 
-        if (Input.GetKey(KeyCode.S))
+        if (Input.GetKey(KeyCode.Space))
         {
-            RB.AddForce(-transform.forward * speed);
-        }
+            initalVelocityY = Mathf.Sqrt(projectileForce) * Mathf.Cos(angle * Mathf.PI / 180.0f);
+            initalVelocityZ = Mathf.Sqrt(projectileForce) * Mathf.Sin(angle * Mathf.PI / 180.0f);
 
-        if (Input.GetKey(KeyCode.A))
-        {
-            RB.AddForce(-transform.right * speed);
-        }
+            comboVelo = new Vector3(0, initalVelocityY, initalVelocityZ);
 
-        if (Input.GetKey(KeyCode.D))
-        {
-            RB.AddForce(transform.right * speed);
+            Debug.DrawLine(transform.position , RB.velocity.normalized);
+            /* initalVelocity = Mathf.Sqrt(projectileForce) * 
+             initalVelocityX = initalVelocity * Mathf.Sin(Mathf.Deg2Rad * angle);
+             initalVelocityZ = initalVelocity * Mathf.Cos(Mathf.Deg2Rad * angle);*/
+
+
+            /*Debug.Log(comboVelo);
+            if (RB.velocity.y <= maxJumpForce)
+            {
+                Debug.Log(RB.velocity.y + "y velo");
+                Mathf.Clamp(RB.velocity.y, 0, maxJumpForce);
+            }*/
+
+            //RB.AddForce((comboVelo * Time.deltaTime * GRAVITY), ForceMode.Impulse);
+            RB.velocity = comboVelo * Time.deltaTime * GRAVITY;
+
+
+
+            /*playerProjectile[i].sprite.Position += new Vector2f(playerProjectile[i].initialVelocity.X * playerProjectile[i].time,
+                                                                       playerProjectile[i].initialVelocity.Y * playerProjectile[i].time
+                                                                       + 0.5f * GRAVITY * playerProjectile[i].time * playerProjectile[i].time
+                                                                       );
+                                                                       */
+
         }
     }
 }
