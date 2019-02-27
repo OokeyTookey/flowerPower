1c4a83be559f1cfc0645737dea0bd1fccb47713a
ANNATOOKEY\annat <anna.tookey@hotmail.com>
2019-02-27 18:24:32 +0400

    Fixed the player controller- Almost done. Need to fix the clamp for the magnitude and change the rotation of the model based on the direction that they are facing.

--- FlowerPower/Assets/Anna/Scripts/AnnaPlayerMovement.cs
+++ FlowerPower/Assets/Anna/Scripts/AnnaPlayerMovement.cs
@@ -7,20 +7,11 @@ public class AnnaPlayerMovement : MonoBehaviour
     Rigidbody RB;
     Vector3 movement;
 
-    public float speed;
-    const float GRAVITY = 9.8f;
-
     float moveXAxis;
     float moveYAxis;
-    public float projectileForce;
+    public float speed;
+    public float maxSpeed;
     public float maxJumpForce;
-
-    [SerializeField]
-    float angle;
-    float initalVelocity;
-    float initalVelocityY;
-    float initalVelocityZ;
-    Vector3 comboVelo;
 
     void Start()
     {
@@ -28,56 +19,39 @@ public class AnnaPlayerMovement : MonoBehaviour
 
         if (RB == null)
         {
-            Debug.LogError("Rigidbody not found!! -A");
+            Debug.LogError("Rigidbody not found!! (Player Movement) -A");
         }
     }
 
     private void FixedUpdate()
     {
+        moveXAxis = Input.GetAxis("Horizontal");
+        moveYAxis = Input.GetAxis("Vertical");
+
         if (RB != null)
         {
             movement = new Vector3(moveXAxis, 0, moveYAxis) * speed;
             RB.AddForce(movement, ForceMode.Acceleration); //Adds a continuous force, utilizing the mass of the object
             //Add Force parameter; Acceleration, Force, Impulse, and VelocityChange.
+
+            if (RB.velocity.magnitude > maxSpeed)
+            {
+                RB.velocity = Vector3.ClampMagnitude(RB.velocity, maxSpeed);
+            }
+
+            if (RB.velocity.y < 0) //Checks if he is falling.   
+            {
+                //Figure out the height of objects and make the force that pulls the player down 
+                RB.velocity += Physics.gravity * Time.deltaTime; //Doubles gravity when the player goes down.
+            }
         }
     }
 
     void Update()
     {
-        moveXAxis = Input.GetAxis("Horizontal");
-        moveYAxis = Input.GetAxis("Vertical");
-
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            initalVelocityY = Mathf.Sqrt(projectileForce) * Mathf.Cos(angle * Mathf.PI / 180.0f);
-            initalVelocityZ = Mathf.Sqrt(projectileForce) * Mathf.Sin(angle * Mathf.PI / 180.0f);
-
-            comboVelo = new Vector3(0, initalVelocityY, initalVelocityZ);
-
-            Debug.DrawLine(transform.position , RB.velocity.normalized);
-            /* initalVelocity = Mathf.Sqrt(projectileForce) * 
-             initalVelocityX = initalVelocity * Mathf.Sin(Mathf.Deg2Rad * angle);
-             initalVelocityZ = initalVelocity * Mathf.Cos(Mathf.Deg2Rad * angle);*/
-
-
-            /*Debug.Log(comboVelo);
-            if (RB.velocity.y <= maxJumpForce)
-            {
-                Debug.Log(RB.velocity.y + "y velo");
-                Mathf.Clamp(RB.velocity.y, 0, maxJumpForce);
-            }*/
-
-            //RB.AddForce((comboVelo * Time.deltaTime * GRAVITY), ForceMode.Impulse);
-            RB.velocity = comboVelo * Time.deltaTime * GRAVITY;
-
-
-
-            /*playerProjectile[i].sprite.Position += new Vector2f(playerProjectile[i].initialVelocity.X * playerProjectile[i].time,
-                                                                       playerProjectile[i].initialVelocity.Y * playerProjectile[i].time
-                                                                       + 0.5f * GRAVITY * playerProjectile[i].time * playerProjectile[i].time
-                                                                       );
-                                                                       */
-
+            RB.AddForce(transform.forward * maxJumpForce, ForceMode.Impulse);
         }
     }
 }
