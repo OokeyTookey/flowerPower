e279d5285d53744f44ec72c0247f8fa4c466aee9
ANNATOOKEY\annat <anna.tookey@hotmail.com>
2019-03-05 22:36:09 +0400

    Simple animations for models, picked a perspective for the camera

--- FlowerPower/Assets/Anna/Scripts/Player Movement/AnnaPlayerMovement.cs
+++ FlowerPower/Assets/Anna/Scripts/Player Movement/AnnaPlayerMovement.cs
@@ -8,16 +8,26 @@ public class AnnaPlayerMovement : MonoBehaviour
 {
     Rigidbody RB;
     Vector3 direction;
+    Vector3 lookPosition;
+    Quaternion lookRotation;
+
+    public Quaternion north;
+    public Quaternion east;
+    public Quaternion south;
+    public Quaternion west;
 
     float moveXAxis;
     float moveYAxis;
-    float mouseXAxis;
-    float mouseYAxis;
+    // float mouseXAxis;
+    // float mouseYAxis;
+
     public float speed;
     public float maxSpeed;
     public float maxJumpForce;
-
     public float rotateSpeed;
+    public float damping;
+    public float moveSpeed;
+    float angle;
 
     void Start()
     {
@@ -29,16 +39,21 @@ public class AnnaPlayerMovement : MonoBehaviour
         moveXAxis = Input.GetAxis("Horizontal");
         moveYAxis = Input.GetAxis("Vertical");
 
-        mouseXAxis += Input.GetAxis("Mouse X");
-        mouseYAxis += Input.GetAxis("Mouse Y");
-
-
-        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, mouseXAxis, transform.rotation.eulerAngles.z);
+        //mouseXAxis += Input.GetAxis("Mouse X");
+        //mouseYAxis += Input.GetAxis("Mouse Y");
 
         direction = (moveXAxis * transform.right + moveYAxis * -transform.up).normalized; //Inverse because model shape
-
+        //direction = (moveXAxis * transform.right + moveYAxis * -transform.up).normalized; //Inverse because model shape
         RB.AddForce(direction * speed, ForceMode.Acceleration); //Adds a continuous force, utilizing the mass of the object
-                                                                //Add Force parameter; Acceleration, Force, Impulse, and VelocityChange.
+                                                                //Add Force parameter; Acceleration, Force, Impulse, and VelocityChange
+                                                                //transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, 90, transform.rotation.eulerAngles.z);
+                                                                //transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, moveYAxis, transform.rotation.eulerAngles.z);
+
+        if (moveXAxis > 0)
+        {
+            transform.rotation = Quaternion.Euler(new Vector3(transform.rotation.eulerAngles.x, -90, transform.rotation.eulerAngles.z));
+        }
+
 
         if (RB.velocity.magnitude > maxSpeed)
         {
@@ -50,7 +65,6 @@ public class AnnaPlayerMovement : MonoBehaviour
             //Figure out the height of objects and make the force that pulls the player down 
             RB.velocity += Physics.gravity * Time.deltaTime; //Doubles gravity when the player goes down.
         }
-
     }
 
     void Update()
@@ -60,4 +74,7 @@ public class AnnaPlayerMovement : MonoBehaviour
             RB.AddForce(transform.forward * maxJumpForce, ForceMode.Impulse); //Move the object forward because its rotated -90
         }
     }
+
+    //transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, mouseXAxis, transform.rotation.eulerAngles.z);
+
 }
