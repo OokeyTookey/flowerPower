577ec4efb70bbe2437b6e10afdb61cb8136b00d8
ANNATOOKEY\annat <anna.tookey@hotmail.com>
2019-03-31 12:32:26 +0400

    Added 4 different types of main menu camera styles. Working on refining one atm.

---
+++ FlowerPower/Assets/Anna/Scripts/Camera/MainMenuSystem2.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuSystem2 : MonoBehaviour
+{
+    public GameObject[] menuCameraPositions;
+    public GameObject[] menuPanels;
+    public float percentage;
+    private int index;
+    bool stopLooping;
+
+    public float speed;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            index++;
+
+            if (index >= menuCameraPositions.Length)
+            {
+                index = 0; //First index in the array (Looping effect)
+            }
+            stopLooping = false;
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            index--;
+            if (index < 0)
+            {
+                index = menuCameraPositions.Length-1; //Last index in the array (Looping effect)
+            }
+            stopLooping = false;
+        }
+
+        
+        for (int i = 0; i < menuCameraPositions.Length; i++)
+        {
+            transform.position = Vector3.Lerp(transform.position, menuCameraPositions[index].transform.position, percentage * Time.deltaTime);
+                transform.rotation = Quaternion.Slerp(transform.rotation, menuCameraPositions[index].transform.rotation, percentage * Time.deltaTime);
+
+            
+        }
+    }
+}
---
+++ FlowerPower/Assets/Anna/Scripts/Camera/RotateAroundPoint.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateAroundPoint : MonoBehaviour
+{
+    //In order to use this script you must attach an empty game object to the scene.
+    //Place the empty game object where you want to rotate around.
+    //Make the camera a child on this object.
+    public float speed;
+
+    void Update()
+    {
+        transform.Rotate(0, speed * Time.deltaTime, 0); //Only rotate on the Y Axis.
+    }
+}
