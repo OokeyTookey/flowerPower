c1691f482ee9de608be803c839a895a032bef06c
merksilver <marksprod@gmail.com>
2019-04-10 18:13:11 +0400

    Added bin, window, gas mask props in 'Mark Gbox' folder. Jarrett added his version of the greybox in "Jarrett Gbox' folder.

---
+++ FlowerPower/Assets/jarrett.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jarrett : MonoBehaviour
+{
+    public float force;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            transform.position +=  new Vector3(0,1 * force,0) * Time.deltaTime;
+
+        }
+    }
+}
