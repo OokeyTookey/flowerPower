65b8f45853f811b659e3a99301abf1e9d5b140bc
merksilver <marksprod@gmail.com>
2019-04-19 13:01:37 +0400

    Added new wall and floor to MarkGbox

---
+++ FlowerPower/Bloomsday/Assets/Player1.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1 : MonoBehaviour
+{
+    public float speed;
+    public float speed2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(0, 0, speed);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(0, 0, -speed);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(-speed, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(speed, 0, 0);
+        }
+
+    
+  
+  
+    
+    }
+}
+
+
