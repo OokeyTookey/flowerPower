2067be38b00a84f931d0db22862269dee33e21fd
ANNATOOKEY\annat <anna.tookey@hotmail.com>
2019-04-21 14:11:18 +0400

    PISTONS WORK, using animation stuff

--- FlowerPower/Assets/2.Anna/8.Scripts/Puzzles/Level1Puzzles.cs
+++ FlowerPower/Assets/2.Anna/8.Scripts/Puzzles/Level1Puzzles.cs
@@ -2,45 +2,84 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class Piston
+{
+    int index;
+    public float speed;
+    [HideInInspector] public Vector3 startPos;
+    [HideInInspector] public Vector3 direction;
+    [HideInInspector]public GameObject gameObject;
+    [HideInInspector] public Vector3 targetPos;
+    [HideInInspector] public bool goToEnd = true;
+}
+
+
 public class Level1Puzzles : MonoBehaviour
 {
-    //public GameObject button1;
-   // public GameObject button2;
-    bool puzzle1Complete;
-    //bool puzzle2Complete;
 
-    public GameObject[] pistons;
-    Transform [] endPos;
-   // public 
-    Transform[] startPos;
+    public bool puzzle1Complete;
+     public bool puzzle2Complete;
+     public Piston[] pistons1;
+
+    public GameObject piston;
+    public GameObject endPos;
+    public GameObject startPos;
     public float speed;
 
-    void Start()
-    {
-        for (int i = 0; i < pistons.Length; i++)
+     void Start()
+     {
+        /*for (int i = 0; i < pistons1.Length; i++)
         {
-            startPos[i].transform.position = pistons[i].transform.position;
-        }
+           pistons1[i].startPos = startPos.transform.position;
+        }*/
+
+        //piston.transform.position = Vector3.MoveTowards(transform.position, endPos.transform.position, speed * Time.deltaTime); //Moves towards the temp current waypoint
+
     }
 
     void Update()
+     {
+         if (!puzzle1Complete)
+         {
+             for (int i = 0; i < pistons1.Length; i++)
+             {
+                 MovePiston(pistons1[i].gameObject, startPos.transform.position, endPos.transform.position);
+             }
+         }
+     }
+
+   void MovePiston(GameObject piston, Vector3 startPos, Vector3 endPos)
     {
-        if (!puzzle1Complete)
+        Vector3 currentPosition;
+        currentPosition = startPos;
+        piston.transform.position = Vector3.MoveTowards(piston.transform.position, currentPosition, speed *Time.deltaTime); //Moves towards the temp current waypoint
+
+        if (Vector3.Distance(piston.transform.position, currentPosition) <= 1)
         {
-            for (int i = 0; i < pistons.Length; i++)
-            {
-                MovePiston(pistons[i], startPos[i], endPos[i]);
-            }
+            currentPosition = endPos;
+            piston.transform.position = Vector3.MoveTowards(piston.transform.position, currentPosition, speed *Time.deltaTime); //Moves towards the temp current waypoint
+
         }
     }
-
-    void MovePiston(GameObject moveGameObject, Transform startPos,Transform endPos)
-    { 
-        moveGameObject.transform.position = Vector3.MoveTowards(moveGameObject.transform.position, endPos.position, Time.deltaTime * speed); //Moves towards the temp current waypoint
-
-        if (Vector3.Distance(moveGameObject.transform.position, endPos.position) <= 1)
-        {
-            moveGameObject.transform.position = Vector3.MoveTowards(moveGameObject.transform.position, startPos.position, Time.deltaTime * speed); //Moves towards the temp current waypoint
-        }   
-    }
 }
+
+/*void MovePiston(Piston piston, Vector3 startPos, Vector3 endPos)
+      {
+          piston.direction = (piston.targetPos - startPos).normalized;
+
+          if (Vector3.Distance(piston.gameObject.transform.position, piston.targetPos) <= 1)
+          {
+              if (piston.goToEnd)
+              {
+                  piston.targetPos = endPos;
+                  piston.goToEnd = false;
+              }
+              else
+              {
+                  piston.targetPos = startPos;
+                  piston.goToEnd = true;
+              }
+          }
+        piston.gameObject.transform.position += piston.direction * piston.speed * Time.deltaTime; //Move the piston
+     }*/
---
+++ FlowerPower/Assets/2.Anna/8.Scripts/Puzzles/Level1PuzzlesAnimations.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level1PuzzlesAnimations : MonoBehaviour
+{
+    GameObject[] pistons;
+    Animator[] anim;
+    private void Start()
+    {
+
+        //loop through all children. Get controllers. 
+        //Set specific trigger to true. 
+        //If button is pressed then set to false
+    }
+
+    private void Update()
+    {
+        
+    }
+}
