50035d0fe75f11a35438bb88c326661fc29bc4ca
ANNATOOKEY\annat <anna.tookey@hotmail.com>
2019-04-25 17:34:47 +0400

    JUMMP WORKS AMAZINGLY IM SO GREAY

--- FlowerPower/Assets/2.Anna/8.Scripts/Player Movement/AnnaPlayerMovement.cs
+++ FlowerPower/Assets/2.Anna/8.Scripts/Player Movement/AnnaPlayerMovement.cs
@@ -4,116 +4,65 @@
 
 public class AnnaPlayerMovement : MonoBehaviour
 {
-    //public Animator animation;
+    [HideInInspector] public Vector3 direction; //Look & movement direction. Used for EVERYTHING (Basically)
 
-    private GameManager gameManager;
-    private PlayerStats playerStats;
+    private float moveXAxis; //Needed for input .
+    private float moveYAxis; //Needed for input 2.0.
 
-    private float moveXAxis;
-    private float moveYAxis;
-
-    private Rigidbody RB;
-    private Collider playerCollider;
-    private Vector3 tempDirection;
-    private Vector3 movementClamp;
-
-    [HideInInspector] public Vector3 direction;
+    private Rigidbody RB; //For player rigidbody.
+    private Collider playerCollider; //For player collider to check if grounded.
 
     [Header("//------ Sunny main values ------")]
-    public float range;
-    public float speed;
-    public float maxSpeed;
-    public float slerpSpeed;
-    public float walkMax;
+    public float speed; //Player walk speed
+    public float rotationSlerpSpeed; //Rotation speed
 
     [Space]
 
     [Header("//------ Jump related ------")]
-    public float clampJumpForce;
-    public float maxJumpForce;
-    public float pullDownForce;
-    public LayerMask groundLayer;
-    bool grounded;
-
-    [HideInInspector]public bool invertControls;
+    public float jumpHeight; //Sets the height of the jump
+    public float timeTillMaxHeight; // how long it takes to reach the max jump height
+    public Vector3 fakeGravity; //Sunny has his own gravity!! :D (dont want to effect everyhting in the scene)
+    public LayerMask groundLayer; //Which layer does sunny check collisions between for jumping
 
     void Start()
     {
-        RB = GetComponent<Rigidbody>();
+        RB = GetComponent<Rigidbody>(); 
         playerCollider = GetComponent<Collider>();
-        gameManager = FindObjectOfType<GameManager>();
-
-        // transform.position = gameManager.lastCheckpointLocation;
     }
 
     private void FixedUpdate()
     {
-        // ---- Take input from the player & Calculate angle
-        moveXAxis = Input.GetAxis("Horizontal"); //Between -1 & 1
-        moveYAxis = Input.GetAxis("Vertical");
+        moveXAxis = Input.GetAxis("Horizontal"); //Takes input from the player.
+        moveYAxis = Input.GetAxis("Vertical"); ////Always between -1 & 1
 
-        // ---- Calcuate the direction using the input then add force.
-        ///Camera.main.transform.position.y = 0;
-        direction = (moveYAxis * new Vector3(Camera.main.transform.forward.x, 0, Camera.main.transform.forward.z)  + moveXAxis * Camera.main.transform.right).normalized;
+        var cameraDirection = Camera.main; // ---- Calcuate the direction using the input & makes it relative to the camera's variables.
+        direction = (moveYAxis * new Vector3(cameraDirection.transform.forward.x, 0, cameraDirection.transform.forward.z)  + moveXAxis * cameraDirection.transform.right).normalized;
 
-        // ---- if there is some input then rotate the object.
-        if (direction.magnitude != 0)
+        if (direction.magnitude != 0) //If there is input, then rotate object based on direction.
         {
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), Time.deltaTime * slerpSpeed);
-          
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), Time.deltaTime * rotationSlerpSpeed); 
         }
-        var velovcity = direction * speed;
-        velovcity.y = RB.velocity.y;
-        RB.velocity=velovcity; //Adds a continuous force, utilizing the mass of the object 
-        //FORCEMODE.ACCELERATION has 4 alt options: Acceleration, Force, Impulse, and VelocityChange                                                   
-        // ---- Clamping the speed
-        float vx = Mathf.Clamp(RB.velocity.x, -walkMax, walkMax);
-        float vz = Mathf.Clamp(RB.velocity.z, -walkMax, walkMax);
-        //RB.velocity = new Vector3(vx, RB.velocity.y, vz);
 
-        if (RB.velocity.y < -.1f )  //Checks if he is falling and double gravity  
-        {
-            RB.velocity += (Physics.gravity * pullDownForce) ; //Doubles gravity when the player goes down. 
-        }      
-    }
-    
-    public void Update()
-    {
+        var moveWithVelo = direction * speed; //Creating new varible for player movement. 
+        moveWithVelo.y = RB.velocity.y; //making sure we are not messing with the Y & it stays the same.
+        RB.velocity = moveWithVelo; //Making the player move using velocity rather than add force! :D
 
-        if (Input.GetButtonDown("Jump") && IsGrounded()) 
+        //Sunny will use his own physics --------------------------------------------------------------------------------------------------
+        fakeGravity = new Vector3(0, -2 * jumpHeight / timeTillMaxHeight / timeTillMaxHeight, 0); //Creating gravity by XX X X X X 
+
+        float jumpForce = 2 * jumpHeight / timeTillMaxHeight;
+
+        if (Input.GetButtonDown("Jump") && IsGrounded())
         {
-            //play jump animation
-            Vector3 jumpDirection = transform.up * maxJumpForce;
-            Vector3 clampedMagnitude = Vector3.ClampMagnitude(jumpDirection, clampJumpForce);
-            RB.AddForce(clampedMagnitude, ForceMode.Impulse);
-            grounded = false;
+            RB.velocity = new Vector3(0, jumpForce, 0); //Adds jump force. 
         }
+        RB.velocity += fakeGravity * Time.deltaTime; //Constantly adds gravity to sunny. 
     }
 
     private bool IsGrounded()
     {
-        
         //CheckCapsule: Will return true if the box colliders/overlaps a specific layer or object.
         return Physics.CheckCapsule(playerCollider.bounds.center, new Vector3(playerCollider.bounds.center.x,
            playerCollider.bounds.min.y, playerCollider.bounds.center.z), .1f /*<- Radius size*/, groundLayer);
     }
-   // int counter=0;
-    /*private void OnCollisionEnter(Collision collision)
-    {
-        counter++;
-        if( Physics.CheckCapsule(playerCollider.bounds.center, new Vector3(playerCollider.bounds.center.x,
-             playerCollider.bounds.min.y, playerCollider.bounds.center.z), .1f /*<- Radius size, groundLayer))
-            grounded = true;
-
-    }
-
-    private void OnCollisionExit(Collision collision)
-    {
-        counter--;
-        if (counter <= 0)
-        {
-            grounded = false;
-            counter = 0;
-        }
-    }*/
 }
--- FlowerPower/Assets/2.Anna/8.Scripts/Player Movement/SunnyMovement.cs
+++ FlowerPower/Assets/2.Anna/8.Scripts/Player Movement/SunnyMovement.cs
@@ -6,12 +6,15 @@ public class SunnyMovement : MonoBehaviour
 {
     public float jumpHeight= 10;
     public float timeTillMaxHeight=.6f;
-    private void Update()
+    public Vector3 gravity;
+    private void FixedUpdate()
     {
-        float jumpForce = (jumpHeight/ timeTillMaxHeight - .5f * Physics.gravity.y * timeTillMaxHeight );
+        gravity = new Vector3(0, -2 * jumpHeight / timeTillMaxHeight / timeTillMaxHeight, 0);
+        float jumpForce = 2 *jumpHeight / timeTillMaxHeight;
         if (Input.GetButtonDown("Jump"))
         {
             this.GetComponent<Rigidbody>().velocity = new Vector3(0, jumpForce, 0);
         }
+        this.GetComponent<Rigidbody>().velocity += gravity*Time.deltaTime; 
     }
 }
