c15aaf90f05878a8bf1a256276761af468948688
ANNATOOKEY\annat <anna.tookey@hotmail.com>
2019-04-26 10:52:49 +0400

    Save + load started. Create a script which currently just stores the player position, rotation & health. *** CHECKPOINTS

---
+++ FlowerPower/Assets/2. Personal Folders/1.Anna/8.Scripts/Save/PlayerDataSave.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+
+public class PlayerDataSave
+{
+    public int currentHealth;
+    public float[] playerPosition;
+    public float[] playerRotation;
+
+    public bool sunFlowerSkillUnlocked;
+    public bool sporeSkillUnlocked;
+    public bool thornsSkillUnlocked;
+
+    //Will need to save the different puzzle elements. 
+
+    public PlayerDataSave(PlayerStats playerStats, AnnaPlayerMovement PlayerMovement, PlayerManager playerManager) //Constructor - We need to date data from the following scripts.
+    {
+        currentHealth = playerStats.currentHealth; //Sets the players current health.
+
+        playerPosition = new float[3]; //Stores player position in an array of floats
+        playerRotation = new float[3]; //Stores player rotation in an array of floats
+
+        playerPosition[0] = PlayerMovement.transform.position.x;
+        playerPosition[1] = PlayerMovement.transform.position.y;
+        playerPosition[2] = PlayerMovement.transform.position.z;
+
+        playerRotation[0] = PlayerMovement.transform.rotation.x;
+        playerRotation[1] = PlayerMovement.transform.rotation.y;
+        playerRotation[2] = PlayerMovement.transform.rotation.z;
+    }
+}
