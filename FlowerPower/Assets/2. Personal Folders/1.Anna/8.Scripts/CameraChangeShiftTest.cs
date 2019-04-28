95a6ed9e7a367ec5e7ddf4f93c4fdf3bc9136a2c
ANNATOOKEY\annat <anna.tookey@hotmail.com>
2019-04-28 20:41:16 +0400

    Lighting etc, new controls. Camera flip, save and load kinda work.

--- FlowerPower/Assets/2. Personal Folders/1.Anna/8.Scripts/CameraChangeShiftTest.cs
+++ FlowerPower/Assets/2. Personal Folders/1.Anna/8.Scripts/CameraChangeShiftTest.cs
@@ -3,68 +3,51 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraChangeShiftTest : MonoBehaviour
-{ }
-  /*  private Vector3 currentCameraPosition;
-
-    private Vector3 startingCameraPosition;
-
-    public GameObject mainCamera;
-    public GameObject playerReference;
-    public GameObject desiredLocation;
-
-    GameObject startingPosition;
+{
+    public float followPlayerSpeed;
+    public float followRotationSpeed;
+    float angle;
+    Vector3 shift;
+    Vector3 currentPosition;
+    Quaternion currentRotation;
     AnnaPlayerMovement playerMovement;
 
-    bool swapCameraNewPer;
-    bool swapCameraOldPer;
-
+        //transform.position = playerMovement.transform.position + shift;
+  
     private void Start()
     {
         playerMovement = FindObjectOfType<AnnaPlayerMovement>();
-        startingCameraPosition = mainCamera.transform.position;
-
-        startingPosition = mainCamera;
+        shift = transform.position - playerMovement.transform.position;
+       // currentRotation = transform.rotation;
     }
 
     private void Update()
     {
-        if (swapCameraNewPer)
+        currentPosition = playerMovement.transform.position + shift;
+        transform.position = Vector3.Lerp(transform.position, currentPosition, followPlayerSpeed * Time.deltaTime);
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, currentRotation, followRotationSpeed* Time.deltaTime);
+
+       // transform.rotation = Quaternion.Slerp(transform.rotation, currentRotation, followRotationSpeed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.P))
         {
-            SwapCameraAngle(mainCamera, desiredLocation);
-            //swapCameraOldPer = false;
+            SwapCameraAngle();
         }
 
-        if (swapCameraOldPer)
-        {
-            SwapCameraAngle(mainCamera, startingPosition);     
-           // swapCameraNewPer = false;
-        }
     }
 
-    private void OnTriggerStay(Collider other)
+    public void SwapCameraAngle()
     {
-        //Vector3 cameraOffset = playerReference.transform.position + startingCameraPosition;
+        shift.x *= -1;
+        //currentRotation.y *= -1;
+        //transform.rotation = Quaternion.Slerp(transform.rotation, rotation,  Time.deltaTime);
+        //transform.Rotate(0, 180, 0, Space.World); 
+        currentRotation = Quaternion.Euler(0, 180, 0) * transform.rotation;
 
-        if (other.CompareTag("Player"))
-        {
-            swapCameraNewPer = true;
-            //playerMovement.invertControls = true;
-        }
-    }
-
-   private void OnTriggerExit(Collider other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            //swapCameraOldPer = true;
-            //playerMovement.invertControls = false;
-        }
-    }
-
-    public void SwapCameraAngle(GameObject mainCamera, GameObject desiredLocation)
-    {
-        //mainCamera.transform.position = Vector3.Lerp(mainCamera.transform.position, desiredLocation.transform.position, 5 * Time.deltaTime);
-        //mainCamera.transform.rotation = Quaternion.Slerp(mainCamera.transform.rotation, desiredLocation.transform.rotation, 5 * Time.deltaTime);
+        //transform.rotation = currentRotation;
+        // mainCamera.transform.position = Vector3.Lerp(mainCamera.transform.position, desiredLocation.transform.position, 5/*REPLACE AFTER TESTIN*/ * Time.deltaTime);
+        // mainCamera.transform.rotation = Quaternion.Slerp(mainCamera.transform.rotation, desiredLocation.transform.rotation, 5 /*REPLACE AFTER TESTIN*/* Time.deltaTime);
     }
 }
-*/
+
