286dfe6c6d01960500a6a02f7f8da9b73cc4f5ec
ANNATOOKEY\annat <anna.tookey@hotmail.com>
2019-04-29 19:56:45 +0400

    Worked on lighting, changed camera perpspective.

--- FlowerPower/Assets/2. Personal Folders/1.Anna/8.Scripts/CameraChangeShiftTest.cs
+++ FlowerPower/Assets/2. Personal Folders/1.Anna/8.Scripts/CameraChangeShiftTest.cs
@@ -6,48 +6,64 @@ public class CameraChangeShiftTest : MonoBehaviour
 {
     public float followPlayerSpeed;
     public float followRotationSpeed;
-    float angle;
+    public float zoomSpeed;
+
     Vector3 shift;
     Vector3 currentPosition;
     Quaternion currentRotation;
     AnnaPlayerMovement playerMovement;
+    RaycastHit hit;
 
-        //transform.position = playerMovement.transform.position + shift;
-  
+    //zoom in based on the closeness of the player to the wall
+    //Access the distance between the wall and plauer
+    //get the difference and move that close
+
     private void Start()
     {
         playerMovement = FindObjectOfType<AnnaPlayerMovement>();
         shift = transform.position - playerMovement.transform.position;
-       // currentRotation = transform.rotation;
     }
 
-    private void Update()
+    private void LateUpdate()
     {
+      /*  var directionToCamera = (transform.position- playerMovement.transform.position).normalized;
+        Ray rayFromPlayer = new Ray(playerMovement.transform.position, directionToCamera);
+
+        if (Physics.Raycast(rayFromPlayer, out hit, LayerMask.GetMask("Wall")))
+        {
+            Debug.DrawRay(playerMovement.transform.position, directionToCamera, Color.green);
+            Debug.Log("collision");
+
+            //var distanceBetween = Vector3.Distance(transform.position, hit.point);
+        }
+        else Debug.Log("no collsion");*/
+      
         currentPosition = playerMovement.transform.position + shift;
         transform.position = Vector3.Lerp(transform.position, currentPosition, followPlayerSpeed * Time.deltaTime);
-
         transform.rotation = Quaternion.Slerp(transform.rotation, currentRotation, followRotationSpeed* Time.deltaTime);
-
-       // transform.rotation = Quaternion.Slerp(transform.rotation, currentRotation, followRotationSpeed * Time.deltaTime);
+        //- ((playerMovement.GetComponent<Rigidbody>().velocity.magnitude>0.01f)?playerMovement.GetComponent<Rigidbody>().velocity.normalized:Vector3.zero );
 
         if (Input.GetKeyDown(KeyCode.P))
         {
             SwapCameraAngle();
         }
-
     }
 
     public void SwapCameraAngle()
     {
         shift.x *= -1;
-        //currentRotation.y *= -1;
-        //transform.rotation = Quaternion.Slerp(transform.rotation, rotation,  Time.deltaTime);
-        //transform.Rotate(0, 180, 0, Space.World); 
         currentRotation = Quaternion.Euler(0, 180, 0) * transform.rotation;
-
-        //transform.rotation = currentRotation;
-        // mainCamera.transform.position = Vector3.Lerp(mainCamera.transform.position, desiredLocation.transform.position, 5/*REPLACE AFTER TESTIN*/ * Time.deltaTime);
-        // mainCamera.transform.rotation = Quaternion.Slerp(mainCamera.transform.rotation, desiredLocation.transform.rotation, 5 /*REPLACE AFTER TESTIN*/* Time.deltaTime);
     }
-}
 
+    /*IEnumerator LerpToPoint(Vector3 point)
+    {
+        var initialPosition=this.transform.position;
+        float t = 0;
+        while (t < 1)
+        {
+        transform.position = Vector3.Lerp(initialPosition, point, t);
+            yield return null;
+            t += 0.01f;
+        }
+    }*/
+}
