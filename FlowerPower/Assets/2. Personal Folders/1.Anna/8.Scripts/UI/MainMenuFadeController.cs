612088bdce8de9cf693034d90de399ce54e2a6c7
ANNATOOKEY\annat <anna.tookey@hotmail.com>
2019-04-30 17:05:06 +0400

    Transitions + Save & Load work

---
+++ FlowerPower/Assets/2. Personal Folders/1.Anna/8.Scripts/UI/MainMenuFadeController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuFadeController : MonoBehaviour
+{
+    TransitionController transitionController;
+    public Animator newGameTransition;
+    public void MainMenuButtonsTransition()
+    {
+        newGameTransition.SetTrigger("end");
+        transitionController.StartCoroutine(transitionController.LoadScene());  //Does generic fade
+    }
+}
