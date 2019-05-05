1f73f4b6b14ec9b0721bfffa17b1761ac560b9f3
ANNATOOKEY\annat <anna.tookey@hotmail.com>
2019-05-05 08:21:20 +0400

    Added timer for the first puzzle. Made player health to 1 at the start of the game. Added mesh colliders to ALLLLLLLL  PROPS in the first level. Added main dialouge & jason bees.

---
+++ FlowerPower/Assets/2. Personal Folders/1.Anna/PlayerHealth1.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlayerHealth1 : MonoBehaviour
+{
+   public PlayerStats playerStats;
+   public TextMeshProUGUI textBoxObjective;
+   public TextMeshProUGUI textBoxTime;
+   private bool startTimer;
+    public float timeTillDeath;
+
+    private void Update()
+    {
+        timeTillDeath -= Time.deltaTime;
+        if (startTimer)
+        {
+            textBoxObjective.text = "Find Water";
+            textBoxTime.text = "Time Remaining: " +  (int)timeTillDeath;
+        }
+
+        if (timeTillDeath <= 0)
+        {
+            playerStats.TakeDamage();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && playerStats.currentHealth >= 1)
+        {
+            startTimer = true;
+            for (int i = 0; i < 5; i++)
+            {
+                playerStats.TakeDamage();
+            }
+        }
+    }
+}
